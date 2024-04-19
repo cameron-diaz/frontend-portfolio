fe5bec32607f4b1cf1d4c4966c3dc952bbc7f959
Cameron Diaz <cdiazit5@gmail.com>
2024-04-18 20:08:48 -0400

    adding more color to background

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} relative bg-gray-50 pt-28 text-gray-950 sm:pt-36`}
       >
-        <div className='absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#e2fbe4] blur-[10rem] sm:w-[68.75rem]'></div>
+        <div className='absolute right-[11rem] top-[-6rem] -z-10 h-[41.25rem] w-[40rem] rounded-full bg-[#e2fbe4] blur-[9rem] sm:w-[68.75rem]'></div>
         <div className='md:left=[-33rem] xl:left=[-15rem] absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] sm:w-[68.75rem] lg:left-[-28rem] 2xl:left-[-5rem]'></div>
         <Header />
         {children}
