ef4883d59ce770f630f2ea16d50e2f8475186c22
Cameron Diaz <cdiazit5@gmail.com>
2024-05-15 12:59:43 -0400

    getting framer motion scroll animation on projects

--- components/project.tsx
+++ components/project.tsx
@@ -2,22 +2,32 @@
 import React from 'react';
 import { projectsData } from "@/lib/data";
 import Image from 'next/image';
-import { useScroll } from 'framer-motion';
+import { useScroll, motion, useTransform } from 'framer-motion';
 import { useRef } from 'react';
+
 
 type ProjectProps = (typeof projectsData)[number];
 
 export default function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  const ref = useRef(null)
-  useScroll({
-    // target: '',
-    offset: ['0 1', '1.33 1'],
+  const ref = useRef<HTMLDivElement>(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['0 1', '1.30 1'],
     // ^ begins when bottom of viewport meets top of target
     // ends when the bottom of the viewport has gone 33% past the end of the target
   });
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.80, 1])
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1])
 
   return (
-    <section className='group relative mb-3 max-w-[42rem] overflow-hidden border-black/5 bg-gray-100 transition last:mb-0 even:pl-8 hover:bg-gray-200 sm:mb-8 sm:h-[20rem] sm:pr-8'>
+    <motion.div ref={ref} style={{
+      scale: scaleProgress,
+      opacity: opacityProgress
+    }}
+    className='mb-[20rem] sm:mb-8 last:mb-0'
+    >
+    <section 
+    className='group relative max-w-[42rem] overflow-hidden border-black/5 bg-gray-100 transition even:pl-8 hover:bg-gray-200 sm:h-[20rem] sm:pr-8 '>
       <div className='flex h-full flex-col px-5 pb-6 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]'>
         <h3 className='text-2xl font-semibold'>{title}</h3>
         <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
@@ -40,5 +50,6 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
         group-even:group-hover:translate-x-2 group-even:group-hover:translate-y-2 group-even:group-hover:rotate-2'
       />
     </section>
+  </motion.div>
   );
 }
