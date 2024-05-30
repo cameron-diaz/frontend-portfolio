d9b81317757209d9f0213c02723127b86ab12a50
Cameron Diaz <cdiazit5@gmail.com>
2024-05-30 14:57:24 -0400

    more contact section styling and functionality

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,16 +1,35 @@
+'use client';
+
 import React from 'react';
 import SectionHeading from './section-heading';
 import { FaPaperPlane } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Contact() {
+    const { ref } = useSectionInView('Contact');
     return (
-        <section
+        <motion.section
+            ref={ref}
             id='contact'
             className='mb-20 w-[min(100%,38rem)] 
-        sm:mb-28'
+            text-center sm:mb-28'
+            initial={{
+                opacity: 0,
+            }}
+            whileInView={{
+                opacity: 1,
+            }}
+            transition={{
+                duration: 1,
+            }}
+            viewport={{
+                once: true,
+            }}
         >
             <SectionHeading>Contact Me</SectionHeading>
-            <p className='text-gray-700'>
+
+            <p className='-mt-4 text-gray-700'>
                 You can contact me directly at{' '}
                 <a
                     className='underline'
@@ -21,16 +40,27 @@ export default function Contact() {
                 or through this form
             </p>
             <form className='mt-10 flex flex-col'>
-                <input type='email' className='borderBlack h-14 rounded-lg' />
-                <textarea className='borderBlack my-3 h-52 rounded-lg p-4' />
+                <input
+                    type='email'
+                    className='borderBlack h-14 rounded-lg px-4'
+                    required
+                    maxLength={500}
+                    placeholder='Your Email'
+                />
+                <textarea
+                    className='borderBlack my-3 h-52 rounded-lg p-4'
+                    required
+                    maxLength={500}
+                    placeholder='Your Message'
+                />
                 <button
                     type='submit'
-                    className='group flex h-[3rem] w-[8rem] items-center justify-center gap-2 rounded-full bg-gray-800 text-white outline-none transition-all hover:scale-110 focus:scale-110 active:scale-105 '
+                    className='group flex h-[3rem] w-[8rem] items-center justify-center gap-2 rounded-full bg-gray-800 text-white outline-none transition-all hover:scale-110 hover:bg-gray-900 focus:scale-110 active:scale-105'
                 >
                     Submit{' '}
                     <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:-translate-y-1 group-hover:translate-x-1' />
                 </button>
             </form>
-        </section>
+        </motion.section>
     );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -7,9 +7,13 @@ import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { FaGithubSquare } from 'react-icons/fa';
 import { useSectionInView } from '@/lib/hooks';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Intro() {
     const { ref } = useSectionInView('Home', 0.5);
+    const { activeSection, setActiveSection, setTimeOfLastClick } =
+        useActiveSectionContext();
+
     return (
         <section
             ref={ref}
@@ -70,6 +74,10 @@ export default function Intro() {
                 <Link
                     href='#contact'
                     className='group flex items-center gap-2 rounded-full bg-gray-900 px-7 py-3 text-white outline-none transition hover:scale-110 hover:bg-gray-950 focus:scale-110 active:scale-90'
+                    onClick={() => {
+                        setActiveSection('Contact');
+                        setTimeOfLastClick(Date.now());
+                    }}
                 >
                     Contact me here{' '}
                     <BsArrowRight className='opacity-70 transition group-hover:translate-x-1' />
